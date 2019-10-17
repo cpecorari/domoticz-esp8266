c2dd722031755f1de55e3af40d3a8e3c690195d3
Côme Pecorari <come@pecorari.fr>
2019-10-17 10:18:38 +0200

    Updated comments for readers that are not me :)

--- PZEM-004-Elec.ino
+++ PZEM-004-Elec.ino
@@ -28,14 +28,11 @@ const char* host = "192.168.1.NN";
 const int   port = 8080;
 String authUsername = "xxx";
 String authPassword = "xxx";
-const int   watchdog = 120000; // Fréquence d'envoi des données à Domoticz - Frequency of sending data to Domoticz -- 120000 is enough !
+const int   watchdog = 120000; // Frequency of sending data to Domoticz -- 120000 is enough !
 unsigned long previousMillis = millis();
 HTTPClient http;
 
 PZEM004Tv30 pzem(3, 1);           // Attach PZEM to hwserial
-
-//PZEM004T pzem(3,1);  // (RX,TX)   (D9,D10) NodeMCU     connect to TX,RX of PZEM    /// Defaults pins Serial port   = PZEM004T pzem(&Serial);  // RX,TX
-//IPAddress ip(192,168,1,2);
 
 bool pzemrdy = false;
 float i_=0; 
@@ -66,7 +63,7 @@ void setup ()
   Debug.begin(HOST_NAME);
   Debug.showColors(true); // Colors
 
-// TUTORIAL http://pdacontrolen.com/electricity-consumption-meter-peacefair-pzem-004-esp8266-arduino-nano/
+// TUTORIAL used partly for PZEM http://pdacontrolen.com/electricity-consumption-meter-peacefair-pzem-004-esp8266-arduino-nano/
 
 }
 
@@ -90,8 +87,8 @@ void loop ()
   if(e >= 0.0){e_=e;}
   debugA("e_ value : %f", e);
 
-   // id WL1 = 428 for conso & power // 454 for Voltage
-   // BORDEAUX = VDD // JAUNE = GND // bleu RX // vert TX
+   // My ids in DOMOTICZ : id WL1 = 428 for conso & power // 454 for Voltage ---> Still don't know how to choose IDs in Domoticz... So change it here when you created your dummy device in Domoticz !
+   // (My colors to remember) DONT FORGET to 'invert' TX and RX between PZEM and ESP8266 == BORDEAUX = VDD // JAUNE = GND // bleu RX // vert TX
   if(WiFi.status() == WL_CONNECTED) {
     String url = "/json.htm?type=command&param=udevice&idx=454&nvalue=0&svalue=";
     url.concat(v_);
