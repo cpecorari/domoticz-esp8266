d18e4c7fac0d565bc5614a52e0064037634b0c3a
Côme Pecorari <come@copristo.com>
2020-06-10 15:27:59 +0200

    Added ArduinoJson library for getting fav devices from Domoticz and sending data from name with idx retrieved

--- UltraSonHumiESP.ino
+++ UltraSonHumiESP.ino
@@ -1,27 +1,30 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 #include "base64.h"
 
 /*
 ********************************************
-14CORE ULTRASONIC DISTANCE SENSOR CODE TEST
+Get distance and humidity from GPIO pins (Analog and Digital)
+Then get favorite devices from Domoticz locally
+And finally send values to corresponding idx in Domoticz from given names
 ********************************************
 */
-// defines pins numbers
 
-const int trigPin = 4;  //D2 --- yes
-const int echoPin = 0;  //D3 --- yes
+// defines pins numbers
+const int trigPin = 4;  //D2
+const int echoPin = 0;  //D3
 const int analogInPin = A0;
 
 // WiFi parameters to be configured
 const char* ssid = "xxx";
 const char* password = "xxx";
 
-const char* host = "192.168.1.NN";
+const char* host = "192.168.1.x";
 const int   port = 8080;
 String authUsername = "xxx";
 String authPassword = "xxx";
-const int   watchdog = 120000; // Fréquence d'envoi des données à Domoticz - Frequency of sending data to Domoticz -- 120000 is enough !
+const int   watchdog = 60000; // Frequency for sending data to Domoticz -- 60000 is enough (1 minute), could be less
 HTTPClient http;
 
 // defines variables
@@ -57,9 +60,13 @@ void loop() {
   if(WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi not connected !");
   } else {  
-    String url = "/json.htm?type=command&param=udevice&idx=423&nvalue=0&svalue=";
-    url += humidity;
-    sendDomoticz(url);
+    String nameIdx = "HumTomato2";
+    int idxHum = getIdx(nameIdx);
+    String url = "/json.htm?type=command&param=udevice&idx=" + String(idxHum) + "&nvalue=0&svalue=" + String(humidity);
+    if(idxHum > 0) 
+    {
+      sendDomoticz(url);
+    }
   }
   
   // Clears the trigPin
@@ -80,13 +87,61 @@ void loop() {
   
   if(WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi not connected !");
-  } else {  
-    String url = "/json.htm?type=command&param=udevice&idx=279&nvalue=0&svalue=";
-    url += distance;
-    sendDomoticz(url);
+  } else { 
+    String nameIdx = "WaterTomato2";
+    int idxDist = getIdx(nameIdx);
+    Serial.println("Got Idx ? ");
+    Serial.println(idxDist);
+    String url = "/json.htm?type=command&param=udevice&idx=" + String(idxDist) + "&nvalue=0&svalue=" + String(distance);
+    if(idxDist > 0) 
+    {
+      sendDomoticz(url);
+    }
   }
 
  delay(watchdog);
+}
+
+int getIdx(String idxName){
+  DynamicJsonDocument jsonDoc(20480);
+  // 20480 Should be enough for about 20 favorite devices in Domoticz (around 1024 per device), go to https://arduinojson.org/v6/assistant/
+  Serial.print("Getting name :");
+  Serial.println(idxName);
+  Serial.print("Requesting URL: ");
+  String url = "/json.htm?type=devices&used=true&filter=all&favorite=1"; // To get favorites devices in Domoticz, refer to doc to filter
+  http.begin(host,port,url);
+  String auth = base64::encode(authUsername + ":" + authPassword);
+  http.addHeader("Authorization", "Basic " + auth);
+  int httpCode = http.GET();
+    if (httpCode) {
+      if (httpCode == 200) {
+        String payload = http.getString();
+        Serial.print("Domoticz get Json response "); 
+        DeserializationError err = deserializeJson(jsonDoc, payload);
+        if (err) {
+          Serial.print(F("deserializeJson() failed with code "));
+          Serial.println(err.c_str());
+        }
+        JsonArray arr = jsonDoc["result"].as<JsonArray>();
+        Serial.print("Domoticz array result Json ? ");
+        Serial.println(arr);
+        for (JsonObject repo : arr) {
+          const String name = repo["Name"];
+          if(name == idxName)
+          {
+            Serial.print("FOUND Name idx !");
+            Serial.println(repo);
+            return repo["idx"];
+          }
+        }
+      }
+      else{
+        Serial.println("Domoticz request error..."); 
+        return 0;
+      }
+    }
+  Serial.println("closing connection");
+  http.end();
 }
 
 void sendDomoticz(String url){
